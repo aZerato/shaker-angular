8a29d81c4d8ca0355389c415d373e3ec07b8607c
Jean-Phi <azerato.graphe@gmail.com>
2020-04-26 20:01:27 -0400

    theme flatpickr + add icons header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,6 +2,20 @@ import { Component, OnInit, ViewEncapsulation, OnDestroy } from '@angular/core';
 import { AuthenticationService } from 'src/app/users/services/authentication.service';
 import { Subscription } from 'rxjs';
 
+import { 
+  faCalendar, 
+  faSquareRootAlt, 
+  faMap, 
+  faDumbbell, 
+  faRocket, 
+  faUser,
+  faHandPeace,
+  faSignInAlt,
+  faSignOutAlt,
+  faRuler,
+  IconDefinition
+} from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -12,6 +26,18 @@ export class HeaderComponent implements OnInit, OnDestroy
 {
   title:string = "Shaker";
   isMenuCollapsed: boolean = true;
+
+  faCalendar: IconDefinition = faCalendar;
+  faSquareRootAlt: IconDefinition = faSquareRootAlt;
+  faMap: IconDefinition = faMap;
+  faDumbbell: IconDefinition = faDumbbell;
+  faRocket: IconDefinition = faRocket;
+  faUser: IconDefinition = faUser;
+  faHandPeace: IconDefinition = faHandPeace;
+  faSignInAlt: IconDefinition = faSignInAlt;
+  faSignOutAlt: IconDefinition = faSignOutAlt;
+  faRuler: IconDefinition = faRuler;
+
 
   private authenticationStatusChangeSubsc: Subscription;
   isAuthenticated: boolean = false;
